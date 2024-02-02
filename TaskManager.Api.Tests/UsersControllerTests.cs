9f0cbb3b76781b3538459846836367161a0afbbf
Fedia Hec <gtwofeed@gmail.com>
2024-02-02 16:16:27 +0300

    +-

---
+++ TaskManager.Api.Tests/UsersControllerTests.cs
@@ -0,0 +1,11 @@
+namespace TaskManager.Api.Tests
+{
+    public class UsersControllerTests
+    {
+        [Fact]
+        public async Task CheckStatus_SendRequest_ExpectedOk()
+        {
+
+        }
+    }
+}
