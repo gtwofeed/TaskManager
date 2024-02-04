36f5dee47e50ac6165d1eae67836d97985ee9c44
Ruslan <Ruslan@PC59>
2024-02-04 19:22:43 +0300

    add common httpclient

--- TaskManager.Api.Tests/UsersControllerIntegration .cs
+++ TaskManager.Api.Tests/UsersControllerIntegration .cs
@@ -1,19 +1,28 @@
 using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using System.Net;
 using Xunit;
+using Microsoft.Extensions.Configuration;
 
 
 namespace TaskManager.Api.Tests
 {
-    public class UsersControllerIntegration
+    public class UsersControllerIntegration : IClassFixture<WebApplicationFactory<Program>>
     {
+        readonly HttpClient client; // единный контекст для всех тестов
+        
+        public UsersControllerIntegration(WebApplicationFactory<Program> application)
+        {
+            client = application.CreateClient();
+        }
+
         [Fact]
         public async Task CheckStatus_SendRequest_ShouldOk()
         {
-
-            await using var application = new WebApplicationFactory<Program>();
-            using var client = application.CreateClient();
 
             // Act
             var response = await client.GetAsync("api/users/check-status");
