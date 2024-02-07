ecbea8e628c9d65c24bd14b12bb6b5ecb23a0891
Fedia Hec <gtwofeed@gmail.com>
2024-02-07 10:40:29 +0300

    +-

--- TaskManager.Api.Tests/UsersControllerIntegration .cs
+++ TaskManager.Api.Tests/UsersControllerIntegration .cs
@@ -1,12 +1,7 @@
 using FluentAssertions;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using System.Net;
 using Xunit;
-using Microsoft.Extensions.Configuration;
 
 
 namespace TaskManager.Api.Tests
--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -50,23 +50,21 @@ namespace TaskManager.Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder?.Build();
-            if (app != null)
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
             {
-                // Configure the HTTP request pipeline.
-                if (app.Environment.IsDevelopment())
-                {
-                    app.UseSwagger();
-                    app.UseSwaggerUI();
-                }
-
-                app.UseAuthentication();
-                app.UseAuthorization();
-
-                app.MapControllers();
-
-                app.Run();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }        
     }
 }
